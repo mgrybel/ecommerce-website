069094a2f3b4c51c4474a1b5b80d21aa101a36c9
mgrybel <grybel.m.m@gmail.com>
2024-04-22 19:05:12 +0200

    Updated the page router

--- frontend/src/routers/pageRouter/PageRouter.jsx
+++ frontend/src/routers/pageRouter/PageRouter.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Home from '@/pages/common/Home';
 import Login from '@/pages/common/Login';
 import Register from '@/pages/customer/Register';
 import SearchListing from '@/pages/common/SearchListing';
@@ -8,6 +9,7 @@ import UserRouter from '@/routers/userRouters/UserRouter';
 const PageRouter = () => {
   return (
     <Routes>
+      <Route exact path='/' element={<Home />} />
       <Route exact path='/login' element={<Login />} />
       <Route exact path='/register' element={<Register />} />
       <Route
